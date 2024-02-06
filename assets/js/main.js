35b25ad8ec1f9acd252df6eba8964baf86256901
titohajili <maqatoto963@gmail.com>
2024-02-06 23:16:57 +0400

    Buttons

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,3 +14,14 @@ const  navClose = document.getElementById('nav-close');
             navMenu.classList.remove('show-menu')
         })
       }
+
+      // Remove Menu Mobile
+
+      const navLink = document.querySelectorAll('.nav__link')
+
+      const linkAction = () => {
+        const navMenu = document.getElementById('nav-menu')
+
+        navMenu.classList.remove('show-menu')
+      }
+      navLink.forEach(n => n.addEventListener('click', linkAction));
